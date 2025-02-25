dc67004e44cd043fce36c6586a9249f97a36e0ee
CristianInBits <128726794+CristianInBits@users.noreply.github.com>
2025-02-25 01:27:10 +0100

    Crear proyecto ejemplos-mustache (punto 0)

---
+++ ejemplos-mustache/src/test/java/com/example/springmvc/ejemplos_mustache/EjemplosMustacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmvc.ejemplos_mustache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemplosMustacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
